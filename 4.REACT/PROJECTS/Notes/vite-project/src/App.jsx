48bffd9330cc1ab1d67d1107956cfb8e19bbe94c
Rishi Mitttal <rishi11mital@gmail.com>
2025-06-06 11:41:30 +0530

    react

---
+++ 4.REACT/PROJECTS/Notes/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import styles from './App.module.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className={styles.heading}>NOTES</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
